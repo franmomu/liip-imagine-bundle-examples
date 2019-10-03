14b4881d79841c449604ec1726c32f65b9828a84
Fran Moreno <franmomu@gmail.com>
2019-10-03 07:37:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Liip Imagine Bundle Examples
+
+The aim of this repository is to test out some configurations
+
+## Setup
+
+
+```bash
+git clone https://github.com/franmomu/liip-imagine-bundle-examples.git
+cd liip-imagine-bundle-examples
+composer install
+bin/console server:start
+```
---
+++ config/bundles.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+];
